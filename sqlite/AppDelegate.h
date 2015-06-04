f61d213695ce72148fd3d2d5560355e354679d0f
Ioan <ioan@SJU-Mac-Lab-10.local>
2015-06-04 20:01:15 +0800

    Initial Commit

---
+++ sqlite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sqlite
+//
+//  Created by Ioan on 6/4/15.
+//  Copyright (c) 2015 Ioan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sqlite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sqlite
+//
+//  Created by Ioan on 6/4/15.
+//  Copyright (c) 2015 Ioan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
